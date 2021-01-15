8216d8b7f83acf7bfd40fb59d5581530c8bf2f6c
pranav verma <pranav.verma1993@gmail.com>
2021-01-15 23:34:43 +0530

    internasnalization of many language in child module

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from '../shared/Services/auth.service';
 
 @Component({
@@ -15,7 +16,15 @@ export class AdminComponent implements OnInit {
   }
   ngOnInit(): void {
   }
-  constructor(private service : AuthService,private router : Router) { }
+  constructor(private service : AuthService,private router : Router,public translate: TranslateService) {
+    translate.addLangs(['English', 'French', 'German']);
+    translate.setDefaultLang('English');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/English|French | German/) ? browserLang : 'English');
+
+
+   }
   logout(){
     this.service.logout()
     }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,13 +5,27 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { SharedModule } from '../shared/shared.module';
 import { FirstComponent } from './first/first.component';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [AdminComponent, FirstComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule,SharedModule,FormsModule
+    AdminRoutingModule,SharedModule,FormsModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+   
   ]
 })
 export class AdminModule { }
